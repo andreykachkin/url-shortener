22bb5979d36846e80b128070ea40cf015875ce91
andreykachkin <angelos2009@mail.ru>
2016-09-20 22:19:10 +0300

    start server, connect modules, create configDB.js and routes.js

---
+++ api/routes.js
@@ -0,0 +1,7 @@
+var bodyParser = require('body-parser');
+var config = require('./configBD');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+module.exports = function (app) {
+
+};
--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var config = require('./api/configBD');
+var MongoStore = require('connect-mongo')(session);
+var port = process.env.PORT || 3000;
 
-
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
 
 var app = express();
+
+app.use(logger('dev'));
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keyboard cat',
+    key: 'sid',
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        maxAge: null
+    },
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('./api/routes')(app);
+
+app.listen(port, function(){
+   console.log('Server listening on port 3000')
+});
